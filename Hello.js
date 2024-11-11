7032ba1cb6a45d0d5b44a0e67ba080e716a6681f
shreyashbandekar <shreyash.bandekar2015@gmail.com>
2024-11-11 18:16:12 +0530

    Module Exports Method

--- Hello.js
+++ Hello.js
@@ -1,2 +1,5 @@
 console.log("hello");
 console.log('Hey There! I am JS Developer');
+const math = require("./math.js");
+console.log('Math value', math.addFn(10, 20));
+console.log('Math value',math.subFn(10, 20));
