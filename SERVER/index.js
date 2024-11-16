0039286a884eea0b4daef998fa4d5d13004641af
shreyashbandekar <shreyash.bandekar2015@gmail.com>
2024-11-16 17:48:20 +0530

    SERVER

--- SERVER/index.js
+++ SERVER/index.js
@@ -5,21 +5,30 @@ const url = require('url');
 
 const myServer = http.createServer((req, res) => {
     if (req.url === './favicon.ico') return res.end('')
-    const log = `${Date.now()}: ${req.url} New Request Received\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
     // @ts-ignore
     const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
+    // console.log(myUrl);
 
     fs.appendFile('log.txt', log, (err, data) => {
 
         switch (myUrl.pathname) {
             case '/':
-                res.end("Home Page");
+                if (req.method === "GET")
+                    res.end("Home Page");
                 break;
             case '/about':
-                const username =myUrl.query.myname;
+                const username = myUrl.query.myname;
                 res.end(`Hi,${username}`);
                 // res.end("I am Shreyash Bandekar");
+                break;
+            case "/signup" :
+                if(req.method === "GET") res.end("Sign Up Page");
+                else
+                if(req.method === "POST"){
+                    //DB Query
+                    res.end("success");
+                }
                 break;
             default:
                 res.end("404 Not Found");
